52c8b0d7e040447a958e2f866a156a6c93132cec
raj-mistry <56315897+raj-mistry@users.noreply.github.com>
2020-11-18 18:28:38 -0500

    added to game, made the game resetable

--- app/src/main/java/com/example/blackjack/Game.java
+++ app/src/main/java/com/example/blackjack/Game.java
@@ -15,22 +15,29 @@ public class Game extends AppCompatActivity {
     protected int[] Deck = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52};
     protected ArrayList<Integer> userArray = new ArrayList<Integer>();
     protected ArrayList<Integer> dealerArray = new ArrayList<Integer>();
-    protected int currentIndex = 0;
-    protected int userTotal = 0;
-    protected int dealerTotal = 0;
-    protected boolean winStatus=false;
+    protected int currentIndex = 0; //the card the program retrieves depends on the index. current index ex: Deck[7] will return 6, but when the deck is shuffled, itll return whatever value is there.
+    protected int userTotal = 0; //user total is the total value of the pile ex: 5+6=11, loses if userTotal>21
 
-    protected boolean userBust=false;
-    protected boolean dealerBust=false;
+    protected int dealerTotal = 0; //same as user total except this is for the dealer AI
+    protected boolean winStatus=false; // true if the player wins, false if player loses.
 
+    protected boolean userBust=false; //true if user bust, player loses
+    protected boolean dealerBust=false; //true if dealer bust, player wins, dealer loses.
+
+    // all the text views in the xml,
+    // ___Pile is the integer sum of the cards
+    // ___Cards is the list of cards
+    // gameStatus indicates which player won, if user won or dealer won.
     TextView dealerPile;
     TextView dealerCards;
     TextView userPile;
     TextView userCards;
     TextView gameStatus;
 
+    //buttons self explanatory.
     Button hitMeButton;
     Button stopButton;
+    Button returnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +50,12 @@ public class Game extends AppCompatActivity {
         userCards = (TextView) findViewById(R.id.userCards);
         hitMeButton = findViewById(R.id.hitMeButton);
         stopButton = findViewById(R.id.stopButton);
+        returnButton = findViewById(R.id.returnButton);
         gameStatus = findViewById(R.id.gameStatus);
+        returnButton.setEnabled(false);
 
         Deck = shuffleArray(Deck);
 
-        hitUser();
-        hitUser();
-        hitDealer();
         hitDealer();
 
         hitMeButton.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +74,6 @@ public class Game extends AppCompatActivity {
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                stop();
                 playDealer();
                 if (dealerBust==true){
                     gameStatus.setText("DEALER BUST, PLAYER VICTORY!");
@@ -76,12 +81,39 @@ public class Game extends AppCompatActivity {
                 }
                 else{
                     gameStatus.setText("PLAYER DEFEAT!");
-
                 }
+                stop();
             }
         });
 
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //define what happens on return
+                reset();
+            }
+        });
 
+    }
+    public void reset(){
+        userArray = new ArrayList<Integer>();
+        dealerArray = new ArrayList<Integer>();
+        currentIndex = 0;
+        userTotal = 0;
+        dealerTotal = 0;
+        winStatus=false;
+        userBust=false;
+        dealerBust=false;
+
+        Deck = shuffleArray(Deck);
+        hitDealer();
+        hitUser();
+
+        gameStatus.setText("");
+
+        hitMeButton.setEnabled(true);
+        stopButton.setEnabled(true);
+        returnButton.setEnabled(false);
     }
 
 
@@ -146,6 +178,8 @@ public class Game extends AppCompatActivity {
 
     public void stop(){
         hitMeButton.setEnabled(false);
+        stopButton.setEnabled(false);
+        returnButton.setEnabled(true);
     }
 
 }
